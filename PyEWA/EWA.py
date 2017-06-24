Correct EWA formula

PyEWA/EWA.py
@@ -28,6 +28,8 @@ class EWA:
 
     def __init__(self, base='constant', loss_function='squared', learning_rate=0.1, prior='uniform', support=np.linspace(0, 1, 10)):
         self.support = support
+        # Compute the step between two evaluation of the distribution
+        self.step = (self.support.max() - self.support.min()) / self.support.shape[0]
         self.base = base
         self.loss_function = loss_function
         self.learning_rate = learning_rate
@@ -55,10 +57,10 @@ class EWA:
         """
         loss_by_example = np.array([self.loss_function.loss(y, self.base.evaluate(x)) for x, y in zip(X, Y)])
         W = np.exp(-self.learning_rate *
-                   np.sum(loss_by_example, axis=0) *
-                   self.prior.pdf)
+                   np.sum(loss_by_example, axis=0)) * \
+            self.prior.pdf
 
-        self.distribution.pdf = W / np.sum(W)
+        self.distribution.pdf = (W / np.sum(W)) / self.step
 
     def update_distribution(self, x, y):
         """
@@ -66,10 +68,10 @@ class EWA:
         y: int
         """
         W = np.exp(-self.learning_rate *
-                   self.loss_function.loss(y, self.base.evaluate(x)) *
-                   self.distribution.pdf)
+                   self.loss_function.loss(y, self.base.evaluate(x))) * \
+            self.distribution.pdf
 
-        self.distribution.pdf = W / np.sum(W)
+        self.distribution.pdf = (W / np.sum(W)) / self.step
 
     def update_prior(self):
         self.prior = self.distribution

PyEWA/examples/pure_random.py
@@ -13,7 +13,7 @@ from EWA import EWA
 from scipy.stats import uniform
 
 def get_data():
-    X = uniform.rvs(loc=0, scale=1, size=3000)
+    X = uniform.rvs(loc=0, scale=1, size=50)
     y = X
     X = np.array([[Xi] for Xi in X])
     return (X, y)
@@ -23,7 +23,7 @@ def apply_EWA():
     X = data[0]
     Y = data[1]
 
-    ewa = EWA(support=np.linspace(start=0, stop=1, num=10))
+    ewa = EWA(support=np.linspace(start=0, stop=1, num=1000))
     """ewa.update_distribution(X[0], Y[0])
     print(ewa.distribution.pdf)"""
 