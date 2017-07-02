fb6755aebc459a7310e8b84e2279fc8449087e73
Alexis Rosuel <alexis@hostnfly.com>
2017-07-02 10:06:57 +0200

    Test uniform prior distribution

--- pyewa/tests/test_distributions.py
+++ pyewa/tests/test_distributions.py
@@ -4,30 +4,23 @@ import numpy.testing as npt
 from unittest import TestCase
 
 from pyewa.distributions import Distribution, Uniform
+from pyewa.utils import create_support
 
 
-class TestDistributions(object):
-    def test_init(self):
-        d = Distribution()
-        assert np.all(d.pdf == 1)
-        assert d.pdf.shape == (10, )
-        npt.assert_allclose(d.support, np.linspace(0, 1, 10))
-
-        d = Distribution(support=np.linspace(
-            2, 10, 15), pdf=np.zeros(shape=15))
-        assert np.all(d.pdf == 0)
-        assert d.pdf.shape == (15, )
-        npt.assert_allclose(d.support, np.linspace(2, 10, 15))
-
+"""class TestDistributions(object):
     def test_update_pdf(self):
         d = Distribution()
         d.update_pdf(pdf=np.zeros(shape=10))
-        assert np.all(d.pdf == 0)
+        assert np.all(d.pdf == 0)"""
 
 
 class TestUniform(object):
     def test_init(self):
-        u = Uniform()
-        assert u.lower == 0
-        assert u.upper == 1
-        assert np.all(u.pdf == 1)
+        support = create_support()
+        u = Uniform(support)
+        assert np.all(u.pdf == 0.1)
+
+        support = create_support(
+            lower=0, upper=2, density=20, base_dimension=3)
+        u = Uniform(support=support)
+        assert np.all(u.pdf == 1.25e-4)
